a274a50d1d536b20ae197065dbec4b7968cf42bc
EliasMG <elias.b.tec@gmail.com>
2017-01-09 21:14:05 -0300

    Projeto brewer

---
+++ src/main/java/com/algaworks/brewer/controller/FotosController.java
@@ -0,0 +1,43 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.brewer.DTO.FotoDTO;
+import com.algaworks.brewer.storage.FotoStorage;
+import com.algaworks.brewer.storage.FotoStorageRuunable;
+
+@RestController
+@RequestMapping("/fotos")
+public class FotosController {
+	
+	@Autowired
+	private FotoStorage fotoStorage;
+	
+	@PostMapping
+	public DeferredResult<FotoDTO> upload(@RequestParam("files[]") MultipartFile[] files) {
+		DeferredResult<FotoDTO> resultado = new DeferredResult<>();
+		
+		Thread thread = new Thread(new FotoStorageRuunable(files, resultado, fotoStorage));
+		thread.start();
+		
+		return resultado;
+	}
+	
+	@GetMapping("/temp/{nome:.*}")
+	public byte[] recuperarFotoTemporaria(@PathVariable("nome") String nomeFoto) {
+		return fotoStorage.recuperarFotoTemporaria(nomeFoto);
+	}
+	
+	@GetMapping("/{nome:.*}")
+	public byte[] recuperar(@PathVariable("nome") String nomeFoto) {
+		return fotoStorage.recuperar(nomeFoto);
+	}
+}
---
+++ src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
@@ -0,0 +1,113 @@
+package com.algaworks.brewer.storage.local;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.brewer.storage.FotoStorage;
+
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.name.Rename;
+
+public class FotoStorageLocal implements FotoStorage {
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(FotoStorageLocal.class);
+	
+	private Path local;
+	private Path localTemporario;
+	
+	public FotoStorageLocal() {
+		this(FileSystems.getDefault().getPath("HOME", ".brewerfotos"));
+		criarPastas();
+	}
+	
+	public FotoStorageLocal(Path path) {
+		this.local = path;
+		criarPastas();
+	}
+
+	private void criarPastas() {
+		try {
+			Files.createDirectories(this.local);
+			this.localTemporario = FileSystems.getDefault().getPath(this.local.toString(), "temp");
+			Files.createDirectories(this.localTemporario);
+			
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Pastas criadas para salvar fotos.");
+				LOGGER.debug("Pasta default: " + this.local.toAbsolutePath());
+				LOGGER.debug("Pasta temporária: " + this.localTemporario.toAbsolutePath());
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Erro criando pasta para salvar foto", e);
+		}
+	}
+
+	@Override
+	public String salvarTemporariamente(MultipartFile[] files) {
+		String novoNome= null;
+		if (files != null && files.length > 0) {
+			MultipartFile arquivo = files[0];
+			novoNome = renomearArquivo(arquivo.getOriginalFilename());
+			try {
+				arquivo.transferTo(new File(this.localTemporario.toAbsolutePath().toString() + FileSystems.getDefault().getSeparator() + novoNome));
+			} catch (Exception e) {
+				throw new RuntimeException("Erro salvando a foto na pasta temporária", e);
+			}
+			
+		}
+		
+		return novoNome;
+	}
+	
+	private String renomearArquivo(String nomeOriginal) {
+		String novoNome = UUID.randomUUID().toString() + "_" + nomeOriginal;
+		
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug(String.format("Nome original: %s, novo nome: %s", nomeOriginal, novoNome));
+		}
+		
+		return novoNome;
+	}
+
+	@Override
+	public byte[] recuperarFotoTemporaria(String nomeFoto) {
+		
+		try {
+			return Files.readAllBytes(this.localTemporario.resolve(nomeFoto));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro lendo a foto temporária", e);
+		}
+	}
+
+	@Override
+	public void salvar(String foto) {
+		try {
+			Files.move(this.localTemporario.resolve(foto), this.local.resolve(foto));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro movendo foto para destino final", e);
+		}
+		
+		try {
+			Thumbnails.of(this.local.resolve(foto).toString()).size(40, 68).toFiles(Rename.PREFIX_DOT_THUMBNAIL);
+		} catch (IOException e) {
+			throw new RuntimeException("Erro gerando thmbnail", e);
+		}
+	}
+
+	@Override
+	public byte[] recuperar(String nomeFoto) {
+		try {
+			return Files.readAllBytes(this.local.resolve(nomeFoto));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro lendo a foto", e);
+		}
+	}
+
+}
