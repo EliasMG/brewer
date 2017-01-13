2209f9f64c69831c8ea28fb5370390059bb84db4
EliasMG <elias.b.tec@gmail.com>
2017-01-13 18:49:31 -0300

    Implementação EstiloFilter

---
+++ src/main/java/com/algaworks/brewer/repository/paginacao/PaginacaoUtil.java
@@ -0,0 +1,27 @@
+package com.algaworks.brewer.repository.paginacao;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaginacaoUtil {
+	
+	public void preparar(Criteria criteria, Pageable pageable) {
+		int paginaAtual = pageable.getPageNumber();
+		int totalRegistroPoePagina = pageable.getPageSize();
+		int primeiroRegistro = paginaAtual * totalRegistroPoePagina;
+		
+		criteria.setFirstResult(primeiroRegistro);
+		criteria.setMaxResults(totalRegistroPoePagina);
+		
+		Sort sort = pageable.getSort();
+		if (sort != null) {
+			Sort.Order order = sort.iterator().next();
+			String field = order.getProperty();
+			criteria.addOrder(order.isAscending() ? Order.asc(field) : Order.desc(field)); 
+		}
+	}
+}
