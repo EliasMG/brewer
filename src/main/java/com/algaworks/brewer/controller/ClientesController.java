61e6e14950bd4239f598f5c78382fb74084d1204
EliasMG <elias.b.tec@gmail.com>
2017-01-15 21:47:24 -0300

    Salvando CPF/CNPF sem formatação

--- src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.algaworks.brewer.model.Cliente;
 import com.algaworks.brewer.model.TipoPessoa;
 import com.algaworks.brewer.repository.Estados;
+import com.algaworks.brewer.service.CadastroClienteService;
 
 @Controller
 @RequestMapping("/clientes")
@@ -21,6 +22,9 @@ public class ClientesController {
 	
 	@Autowired
 	private Estados estados;
+	
+	@Autowired
+	private CadastroClienteService cadastroClienteService;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo(Cliente cliente) {
@@ -38,6 +42,7 @@ public class ClientesController {
 			return novo(cliente);
 		}
 		
+		cadastroClienteService.salvar(cliente);
 		attributes.addFlashAttribute("mensagem", "Cliente salva com sucesso");
 		return new ModelAndView("redirect:/clientes/novo");
 	}
