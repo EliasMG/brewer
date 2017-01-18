b6ea9781d1c533ec7baa55e25ed616e48d1624b6
EliasMG <elias.b.tec@gmail.com>
2017-01-17 22:54:21 -0300

    Cadastro de cidade

---
+++ src/main/java/com/algaworks/brewer/service/exception/NomeCidadeJacadastradoParaEstadoSelecionaException.java
@@ -0,0 +1,10 @@
+package com.algaworks.brewer.service.exception;
+
+public class NomeCidadeJacadastradoParaEstadoSelecionaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public NomeCidadeJacadastradoParaEstadoSelecionaException(String mensage) {
+		super(mensage);
+	}
+}
