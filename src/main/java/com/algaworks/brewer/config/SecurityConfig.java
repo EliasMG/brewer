12fcab7f9bf991f1ee28f31fdd34b5dce0b667ff
EliasMG <elias.b.tec@gmail.com>
2017-01-21 11:32:06 -0300

    Tela de login | Autenticando usuário e senha no banco de dados

--- src/main/java/com/algaworks/brewer/config/SecurityConfig.java
+++ src/main/java/com/algaworks/brewer/config/SecurityConfig.java
@@ -1,20 +1,36 @@
 package com.algaworks.brewer.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.algaworks.brewer.security.AppUserDatailsService;
+
 @EnableWebSecurity
+@ComponentScan(basePackageClasses = AppUserDatailsService.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("admin").password("admin").roles("ADMINISTRADOR");
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+		.antMatchers("/layout/**")
+		.antMatchers("/image/**");
 	}
 	
 	@Override
@@ -23,7 +39,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 				.anyRequest().authenticated()
 				.and()
-			.formLogin();
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.csrf().disable();
 	}
 	
 	@Bean
---
+++ src/main/resources/db/migration/V10__inserir_usuario_administrador.sql
@@ -0,0 +1,3 @@
+INSERT 
+	INTO usuario (nome, email, senha, ativo) 
+	VALUES ('Admin', 'admin@brewer.com', '$2a$10$Pnenrk0vqjGG98Ykxv611uc9XPiyimoYJphX0RVtHvCUeTm8dcqeS', 1)
