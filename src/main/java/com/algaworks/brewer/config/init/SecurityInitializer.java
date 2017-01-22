be279653509b5b8622938528ecd1468a91d15581
EliasMG <elias.b.tec@gmail.com>
2017-01-22 14:04:53 -0300

    Expirando sessão com novo login e duração da sessão

--- src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
+++ src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
@@ -1,7 +1,10 @@
 package com.algaworks.brewer.config.init;
 
+import java.util.EnumSet;
+
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
+import javax.servlet.SessionTrackingMode;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -10,8 +13,10 @@ public class SecurityInitializer extends AbstractSecurityWebApplicationInitializ
 	
 	@Override
 	protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
-		FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodinFilter", new CharacterEncodingFilter());
+		//servletContext.getSessionCookieConfig().setMaxAge(20);
+		servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
 		
+		FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodinFilter", new CharacterEncodingFilter());
 		characterEncodingFilter.setInitParameter("encoding", "UTF-8");
 		characterEncodingFilter.setInitParameter("forceEncoding", "true");
 		characterEncodingFilter.addMappingForUrlPatterns(null, false, "/*");
