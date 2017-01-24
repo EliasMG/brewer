7135a91b050afd2a39847bbebb2008a739684353
EliasMG <elias.b.tec@gmail.com>
2017-01-24 15:30:40 -0300

    Miltiseleção parte 2
    Multiseleção de linhas: ativando/desativando vários usuário. Melhorando
    a usabilidade da página de pesquisa de usuários

--- src/main/resources/static/javascripts/multiselecao.js
+++ src/main/resources/static/javascripts/multiselecao.js
@@ -4,16 +4,56 @@ Brewer.MultiSelecao = (function() {
 	
 	function MultiSelecao() {
 		this.statusBtn = $('.js-status-btn');
-	} 
+		this.selecaoCheckbox = $('.js-selecao');
+		this.selecaoCheckboxTodos = $('.js-selecao-todos');
+	}
 	
 	MultiSelecao.prototype.iniciar = function() {
 		this.statusBtn.on('click', onStatusBtnClicado.bind(this));
+		this.selecaoCheckboxTodos.on('click', onSelecaoCheckboxTodosClicado.bind(this));
+		this.selecaoCheckbox.on('click', onSelecaoClicado.bind(this));
 	}
 	
 	function onStatusBtnClicado(event) {
 		var botaoClicado = $(event.currentTarget);
-		console.log(botaoClicado.data('status'))
-						
+		var status = botaoClicado.data('status');
+		var url = botaoClicado.data('url');
+		
+		var checkBoxSelecionados = this.selecaoCheckbox.filter(':checked');
+		var codigos = $.map(checkBoxSelecionados, function(c) {
+			return $(c).data('codigo');
+		});
+		
+		if (codigos.length > 0) {
+			$.ajax({
+				url: url,
+				method: 'PUT',
+				data: {
+					codigos: codigos,
+					status: status
+				}, 
+				success: function() {
+					window.location.reload();
+				}
+			});
+			
+		}
+	}
+	
+	function onSelecaoCheckboxTodosClicado() {
+		var status = this.selecaoCheckboxTodos.prop('checked');
+		this.selecaoCheckbox.prop('checked', status);
+		statusBotaoAcao.call(this, status);
+	}
+	
+	function onSelecaoClicado() {
+		var selecaoCheckboxChecados = this.selecaoCheckbox.filter(':checked');
+		this.selecaoCheckboxTodos.prop('checked', selecaoCheckboxChecados.length >= this.selecaoCheckbox.length);
+		statusBotaoAcao.call(this, selecaoCheckboxChecados.length);
+	}
+	
+	function statusBotaoAcao(ativar) {
+		ativar ? this.statusBtn.removeClass('disabled') : this.statusBtn.addClass('disabled');
 	}
 	
 	return MultiSelecao;
@@ -21,8 +61,6 @@ Brewer.MultiSelecao = (function() {
 }());
 
 $(function() {
-	
 	var multiSelecao = new Brewer.MultiSelecao();
 	multiSelecao.iniciar();
-	
 });
