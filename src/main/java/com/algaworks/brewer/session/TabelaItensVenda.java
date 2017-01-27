775a653b48283cbe5299c4b13f4cc38f8245f542
EliasMG <elias.b.tec@gmail.com>
2017-01-27 00:10:12 -0300

    Adicionando mesmos itens na venda e atualizando a quantidade de itens

--- src/main/java/com/algaworks/brewer/session/TabelaItensVenda.java
+++ src/main/java/com/algaworks/brewer/session/TabelaItensVenda.java
@@ -3,6 +3,7 @@ package com.algaworks.brewer.session;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
@@ -21,12 +22,29 @@ public class TabelaItensVenda {
 	}
 	
 	public void adicionarIten(Cerveja cerveja, Integer quantidade) {
-		ItemVenda itemVenda = new ItemVenda();
-		itemVenda.setCerveja(cerveja);
-		itemVenda.setQuantidade(quantidade);
-		itemVenda.setValorUnitario(cerveja.getValor());
+		Optional<ItemVenda> itemVendaOptional = buscarItemPorCerveja(cerveja);
 		
-		itens.add(itemVenda);
+		ItemVenda itemVenda = null;
+		if (itemVendaOptional.isPresent()) {
+			itemVenda = itemVendaOptional.get();
+			itemVenda.setQuantidade(itemVenda.getQuantidade() + quantidade);
+		} else {
+			itemVenda = new ItemVenda();
+			itemVenda.setCerveja(cerveja);
+			itemVenda.setQuantidade(quantidade);
+			itemVenda.setValorUnitario(cerveja.getValor());
+			itens.add(0, itemVenda);
+		}
+		
+	}
+
+	private Optional<ItemVenda> buscarItemPorCerveja(Cerveja cerveja) {
+		return itens.stream().filter(i -> i.getCerveja().equals(cerveja)).findAny();
+	}
+	
+	public void alterarQuantidade(Cerveja cerveja, Integer novaQuantidade) {
+		ItemVenda itemVenda = buscarItemPorCerveja(cerveja).get();
+		itemVenda.setQuantidade(novaQuantidade);
 	}
 	
 	public int total() {
--- src/main/resources/static/javascripts/venda.tabela-itens.js
+++ src/main/resources/static/javascripts/venda.tabela-itens.js
@@ -23,6 +23,28 @@ Brewer.TabelaItens = (function() {
 	
 	function onItemAdicionadoNoServidor(html) {
 		this.tabelaCervejasContainer.html(html);
+		$('.js-tabela-cerveja-quantidade-item').on('change', onQuantidadeItemAlterado.bind(this));
+		$('.js-tabela-item').on('dblclick', onDoubleClick);
+	}
+	
+	function onQuantidadeItemAlterado(evento) {
+		var input = $(evento.target);
+		var quantidade = input.val();
+		var codigoCerveja = input.data('codigo');
+		
+		var resposta = $.ajax({
+			url: 'item/' + codigoCerveja,
+			method: 'PUT',
+			data: {
+				novaQuantidade: quantidade
+			}
+		});
+		
+		resposta.done(onItemAdicionadoNoServidor.bind(this));
+	}
+	
+	function onDoubleClick(evento) {
+		$(this).toggleClass('solicitando-exclusao');
 	}
 	
 	return TabelaItens;
