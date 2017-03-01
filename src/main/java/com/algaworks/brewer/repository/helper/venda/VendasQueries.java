855165540da15285d96b01d76209b848e383dbc5
EliasMG <elias.b.tec@gmail.com>
2017-03-01 13:34:44 -0300

    Consultas em arquivos externos

--- src/main/java/com/algaworks/brewer/repository/helper/venda/VendasQueries.java
+++ src/main/java/com/algaworks/brewer/repository/helper/venda/VendasQueries.java
@@ -1,8 +1,12 @@
 package com.algaworks.brewer.repository.helper.venda;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.algaworks.brewer.dto.VendaMes;
 import com.algaworks.brewer.model.Venda;
 import com.algaworks.brewer.repository.filter.VendaFilter;
 
@@ -12,4 +16,9 @@ public interface VendasQueries {
 	
 	public Venda buscarComItens(Long codigo);
 	
+	public BigDecimal valorTotalNoAno();
+	public BigDecimal valorTotalNoMes();
+	public BigDecimal valorTicketMedioNoAno();
+	
+	public List<VendaMes> totalPorMes();
 }
